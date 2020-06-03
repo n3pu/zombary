b9a3869760e18c2f330d4ef901cd1e0d4b1b85d8
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2020-06-03 00:44:12 -0300

    correção no envio do chat e estilos

---
+++ public/chat.js
@@ -0,0 +1,141 @@
+$(document).ready(function() {
+  var container = document.getElementById("chatroom");
+  setTimeout(function(){
+    $('#chatroom').animate({ scrollTop: container.scrollHeight}, 1000);
+  },1000);    
+});
+
+// Get the modal
+var modal = document.getElementById("username-modal");
+const nicknameInput = document.getElementById("username");
+
+// Close modal when nick-name is typed
+nicknameInput.onkeypress = e => {
+  let keycode = (e.keyCode ? e.keyCode : e.which);
+  if(keycode == '13'){
+      modal.style.display = "none";
+  }
+};
+
+$(function () {
+  var socket = io('http://localhost:3000');
+
+  let nickName = $("#username");
+  let send_message = $("#send_message");
+  let message = $("#message");
+  let feedback = $("#feedback");
+  let usersList = $("#users-list");
+  let chatroom = $("#chatroom");
+  let currentUser = $("#current_user");
+
+  //Emit message
+  // If send message btn is clicked
+  send_message.click(function(){
+    socket.emit('new_message', {message : message.val()})
+  });
+
+  // Or if the enter key is pressed
+  message.keypress( e => {
+    let keycode = (e.keyCode ? e.keyCode : e.which);
+    if(keycode == '13'){
+      socket.emit('new_message', {message : message.val()})
+    }
+  })
+
+  //Listen on new_message
+  socket.on("new_message", (data) => {
+    feedback.html('');
+    message.val('');
+    //append the new message on the chatroom
+    chatroom.append(`
+    
+    <div class="">
+      <div class="user-message">
+        <div style='color:${data.color}' class="author">${data.username}</div>
+        <div class="content-message">${data.message}</div>
+      </div>
+    </div>  
+    
+    `)
+    keepTheChatRoomToTheBottom()
+  });
+
+  // function renderMessage(message) {
+  //   var currentAuthor = message.id;
+  //   var userColorClass = "other";
+  //   if( currentAuthor === socket.id ) {
+  //     userColorClass = "self";
+  //   }
+  //   $('.messages').append('<div class="message '+ userColorClass +'"><div class="user-message"><div class="author">'
+  //     + message.author +'</div><div class="content-message">'
+  //     + message.message +'</div></div></div>');
+  // }
+
+  //Emit a username
+  nickName.keypress( e => {
+    let keycode = (e.keyCode ? e.keyCode : e.which);
+    if(keycode == '13'){
+      socket.emit('change_username', {nickName : nickName.val()});
+      currentUser.append(`
+        <div class="current-user">${nickName.val()}</div>
+      `);
+      socket.on('get users', data => {
+        let html = '';
+        for(let i=0;i<data.length;i++){
+          html += `<li class="list-item" style="color: ${data[i].color}">${data[i].username}</li>`;
+        }
+        usersList.html(html)
+      })
+    }
+  });
+
+  //Emit typing
+  message.on("keypress", e => {
+    let keycode = (e.keyCode ? e.keyCode : e.which);
+    if(keycode != '13'){
+      socket.emit('typing')
+    }
+  });
+
+  //Listen on typing
+  socket.on('typing', (data) => {
+    feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+  });
+
+  socket.on('previousMessages', function(messages) {
+    for (message of messages) {
+      renderMessage(message);
+    }
+  });
+
+  socket.on('receiveMessage', function(message) {
+    renderMessage(message);
+    var container = document.getElementById("messages");    
+    $('#messages').animate({ scrollTop: container.scrollHeight}, 1000);
+  });
+
+  // $('#chat').submit(function(event){
+  //   event.preventDefault();
+
+  //   if (nickName.length && message.length) {
+  //     var messageObject = {
+  //       nickName: nickName,
+  //       message: message,
+  //       id: socket.id
+  //     };
+
+  //     renderMessage(messageObject);
+
+  //     socket.emit('sendMessage', messageObject);
+  //   }
+  //   var container = document.getElementById("messages");    
+  //   $('#messages').animate({ scrollTop: container.scrollHeight}, 1000);
+  // });
+
+});
+
+// function thats keeps the chatbox stick to the bottom
+const keepTheChatRoomToTheBottom = () => {
+  const chatroom = document.getElementById('chatroom');
+  chatroom.scrollTop = chatroom.scrollHeight - chatroom.clientHeight;
+}
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+const uuid = require('uuid');
+
+let randomColor = require('randomcolor');
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
@@ -17,9 +21,19 @@ app.use('/', (req, res) => {
 });
 
 let messages = [];
+let users = [];
+let connections = [];
 
 io.on('connection', socket => {
-  console.log(`Socket conectado ${socket.id}`);
+  console.log(`Socket connected ${socket.id}`);
+
+  connections.push(socket)
+
+  //initialize a random color for the socket
+  let color = randomColor();
+
+  socket.username = 'Anonymous';
+  socket.color = color;
 
   socket.emit('previousMessages', messages);
 
@@ -27,6 +41,52 @@ io.on('connection', socket => {
     messages.push(data);
     socket.broadcast.emit('receiveMessage', data);
   });
+
+  //listen on change_username
+  socket.on('change_username', data => {
+    let id = uuid.v4();;
+    socket.id = id;
+    socket.username = data.nickName;
+    users.push({id, username: socket.username, color: socket.color});
+    updateUsernames();
+  });
+
+  //update Usernames in the client
+  const updateUsernames = () => {
+    io.sockets.emit('get users', users);
+  }
+
+  //listen on new_message
+  socket.on('new_message', (data) => {
+    //broadcast the new message
+    io.sockets.emit('new_message', {message : data.message, username : socket.username, color: socket.color});
+  });
+
+
+  //listen on typing
+  socket.on('typing', data => {
+    socket.broadcast.emit('typing',{username: socket.username})
+  });
+
+  //Disconnect
+  socket.on('disconnect', data => {
+
+    if(!socket.username)
+        return;
+    //find the user and delete from the users list
+    let user = undefined;
+    for(let i= 0;i<users.length;i++){
+        if(users[i].id === socket.id){
+            user = users[i];
+            break;
+        }
+    }
+    users = users.filter( x => x !== user);
+    //Update the users list
+    updateUsernames();
+    connnections.splice(connnections.indexOf(socket),1);
+  });
+
 });
 
 server.listen(PORT, () => {
